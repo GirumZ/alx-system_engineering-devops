0b14cfd23ee99add4f765befe7a29524a27993b6
GirumZ <girum.zebe@gmil.com>
2023-11-28 13:10:17 -0800

    [Bug fix]: corrected for puppet-lint

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,5 +2,5 @@
 
 exec { 'Nginx installation':
   provider => shell,
-  command => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html; sudo sed -i '/listen 80 default_server/a rewrite ^/redirect_me https://intranet.alxswe.com/ permanent;' /etc/nginx/sites-available/default; sudo service nginx start',
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html; sudo sed -i \'/listen 80 default_server/a rewrite ^/redirect_me https://intranet.alxswe.com/ permanent;\' /etc/nginx/sites-available/default; sudo service nginx start',
 }
